c988d533055517b5bb2154754f6da6c44ca8620f
joey bennett <joeykb17@gmail.com>
2022-06-21 13:36:13 -0700

    add navbar and hero

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,28 @@ import React from 'react';
 function Header() {
     return (
       <header>
+        <div className="navbar bg-base-100">
+          <div className="flex-1">
+            <a className="btn btn-ghost normal-case text-xl">space weather dashboard</a>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal p-0">
+              <li><a>Item 1</a></li>
+              <li><a>Item 2</a></li>
+              <li><a>Item 3</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="hero min-h-screen" style="background-image: url(https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1742&q=80);">
+          <div class="hero-overlay bg-opacity-60"></div>
+          <div class="hero-content text-center text-neutral-content">
+            <div class="max-w-md">
+              <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+              <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+              <button class="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
       </header>
     );
   }
