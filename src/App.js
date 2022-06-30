18b52d80cf410daa6af4479026fee34dea827c83
coderbennett <jbennett@csumb.edu>
2022-06-30 14:06:12 -0700

    add router, add correct jsx for footer

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 import SpaceFlightNews from './components/SpaceFlightNews';
 import Footer from './components/Footer';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-        <div id="content">
-          <Routes>
-              <Route path="/" element={<Dashboard />}/>
-              <Route path="/space-flight-news" element={<SpaceFlightNews />}/>
-          </Routes>
-        </div>
-      <Footer />
+      <Router>
+        <Header />
+          <div id="content">
+            <Routes>
+                <Route path="/" element={<Dashboard />}/>
+                <Route path="/space-flight-news" element={<SpaceFlightNews />}/>
+            </Routes>
+          </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Footer() {
     return (
-        <footer class="footer footer-center p-4 bg-base-300 text-base-content">
+        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
             <div>
                 <p>Copyright © 2022 - All right reserved by Space Weather Dashboard</p>
             </div>
