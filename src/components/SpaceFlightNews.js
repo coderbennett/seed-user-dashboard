4cf7ca00d4cc0985fe272537906015e65b153bb1
coderbennett <jbennett@csumb.edu>
2022-06-30 15:11:37 -0700

    add fetch and render for articles related to space flight

--- src/components/SpaceFlightNews.js
+++ src/components/SpaceFlightNews.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SpaceFlightNews() {
-    
+
+    const [articleState, setArticleState] = useState([]);
+
+    fetch("https://api.spaceflightnewsapi.net/v3/articles")
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            setArticleState([...data]);
+        });
+
+
+    return (
+        <>
+            <div className="flex flex-wrap justify-center">
+                {articleState && articleState.map((article, i) => (
+                <div key={i} className="card w-96 bg-base-100 shadow-xl mx-auto my-6">
+                    <figure><img src={article.imageUrl} alt={article.title} /></figure>
+                    <div className="card-body">
+                      <h2 className="card-title">{article.title}</h2>
+                      <p>{article.summary}</p>
+                      <div className="card-actions justify-end">
+                        <a href={article.url} target="_blank" rel="noreferrer"><button className="btn btn-primary">View Article</button></a>
+                      </div>
+                    </div>
+                </div>
+                 ))}
+            </div>
+        </>
+    )
 }
 
 export default SpaceFlightNews;
